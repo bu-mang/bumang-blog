b4808e8462d0efa0e47a0ab2a0d7a0a5b05ff5d1
Bumang-Cyber <calmness0729@gmail.com>
2025-01-27 16:16:38 +0900

    feat: 헤더 navigator userAgent state 관리 방식으로 로직로 변경

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,6 +1,6 @@
 const Grid = () => {
   return (
-    <div className="fixed z-10 grid h-full w-full grid-cols-4 gap-[5vw] bg-blue-100 px-[1vw] opacity-50">
+    <div className="fixed z-10 grid h-full w-full grid-cols-4 gap-[1vw] bg-blue-100 px-[3vw] opacity-50">
       <div className="grid h-full grid-cols-2 gap-[1vw] bg-blue-100">
         <div className="h-full bg-blue-300"></div>
         <div className="h-full bg-blue-300"></div>
