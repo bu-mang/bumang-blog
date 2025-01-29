0ceedd5f2e5f52496bd2eb13938400d7e6005fda
Bumang-Cyber <calmness0729@gmail.com>
2025-01-29 23:19:59 +0900

    feat:탭 구조 작업

--- src/components/filterBar/index.tsx
+++ src/components/filterBar/index.tsx
@@ -6,23 +6,12 @@ import Link from "next/link";
 import { cn } from "@/utils/cn";
 import { usePathname } from "next/navigation";
 import gsap from "gsap";
-import { useEffect, useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 const FilterBar = () => {
-  const blogSubCategories = ROUTES.filter(
-    (item) => item.title === "Blog",
-  )[0].sub?.map((item) => item.title);
   const pathnames = usePathname()
     .split("/")
     .filter((item) => item !== "");
-
-  const setUrl = (target: string) => {
-    if (target !== "All") {
-      return `/blog/${target.toLowerCase()}`;
-    }
-    return `/blog`;
-  };
-
   const getTabStyle = (target: string) =>
     cn(
       "text-xl text-gray-300 font-medium",
@@ -30,19 +19,19 @@ const FilterBar = () => {
       pathnames[1] === undefined && target === "All" && "text-black",
     );
 
-  useLayoutEffect(() => {
-    gsap;
-  }, []);
-
   return (
     <div className="mx-[3vw] border-t-[1px] border-gray-10">
-      <div className="absolute h-1 w-6 bg-red" />
+      <div className="TAB_INDICATOR absolute h-1 w-5 bg-red" />
       <div className="flex w-full gap-2 py-2">
-        {blogSubCategories?.map((item) => (
-          <Link href={setUrl(item)} className={getTabStyle(item)}>
-            {item}
-          </Link>
-        ))}
+        <Link href={"/blog"} className={getTabStyle("All")}>
+          All
+        </Link>
+        <Link href={"/blog/dev"} className={getTabStyle("Dev")}>
+          Dev
+        </Link>
+        <Link href={"/blog/life"} className={getTabStyle("Life")}>
+          Life
+        </Link>
       </div>
     </div>
   );
