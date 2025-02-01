984ef5aa617765afc5dd38754ef6a56bca8d037b
Bumang-Cyber <calmness0729@gmail.com>
2025-02-02 01:00:33 +0900

    feat: 카드 컴포넌트 생성, tilting 애니메이션 설정

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,22 +1,13 @@
 "use client";
 
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import WorkCard from "@/components/workItem";
 
 export default function Work() {
-  const height = 200 * 1.5;
-  const width = 700 * 1.5;
-
-  // const proportionClass = clsx(`h-${height}`, `w-${width}`);
-
   return (
-    <main className="flex flex-1 flex-col items-center justify-center">
-      <DotLottieReact
-        src="/bumangtm.lottie"
-        autoplay
-        speed={1.8}
-        style={{ height, width }}
-      />
-      <div>프레텐다드.</div>
+    <main className="flex flex-1 flex-col items-center justify-center px-[3vw]">
+      <WorkCard onClick={() => {}} imgSrc="" imgAlt="" />
+      <WorkCard onClick={() => {}} imgSrc="" imgAlt="" />
+      <WorkCard onClick={() => {}} imgSrc="" imgAlt="" />
     </main>
   );
 }
