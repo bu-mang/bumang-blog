5483006a43154d0b2d9b50f7214ad1430b301947
Bumang-Cyber <calmness0729@gmail.com>
2025-02-03 23:55:02 +0900

    feat: 어바웃 페이지 마크업 시작

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,9 @@
 export default function Blog() {
   return (
     <main className="">
-      <div>blog</div>
+      <div className="grid h-fit w-full grid-cols-8 gap-x-[1vw] gap-y-[2vw] px-[3vw]">
+        <div className="bg-red">about</div>
+      </div>
     </main>
   );
 }
