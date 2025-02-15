c01be325297180b14e116810a3c1beec18eb733a
Bumang-Cyber <calmness0729@gmail.com>
2025-02-15 22:08:15 +0900

    feat: 에디터에 드래그 새로운 기능 핸들 추가 (bulletList, Heading, codeBlock)

--- src/app/blog/edit/page.tsx
+++ src/app/blog/edit/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import { useEditor, EditorContent } from "@tiptap/react";
-import "highlight.js/styles/panda-syntax-dark.css";
+import {
+  useEditor,
+  EditorContent,
+  mergeAttributes,
+  NodeViewWrapper,
+  NodeViewContent,
+  ReactNodeViewRenderer,
+  NodeViewProps,
+  RawCommands,
+} from "@tiptap/react";
 
 /**
  * MARKS
  */
 import Bold from "@tiptap/extension-bold";
-import Code from "@tiptap/extension-code";
 import Highlight from "@tiptap/extension-highlight";
 import Italic from "@tiptap/extension-italic";
 import Link from "@tiptap/extension-link";
@@ -41,6 +48,10 @@ import Text from "@tiptap/extension-text";
 import Paragraph from "@tiptap/extension-paragraph";
 import Document from "@tiptap/extension-document";
 import StarterKit from "@tiptap/starter-kit";
+import { all, createLowlight } from "lowlight";
+import "highlight.js/styles/panda-syntax-dark.css";
+// import "highlight.js/styles/docco.css";
+// import "highlight.js/styles/magula.css";
 
 /**
  * FUNCTIONS
@@ -48,79 +59,302 @@ import StarterKit from "@tiptap/starter-kit";
 import Dropcursor from "@tiptap/extension-dropcursor";
 import ListKeymap from "@tiptap/extension-list-keymap";
 import { Color } from "@tiptap/extension-color";
-import { FontFamily } from "@tiptap/extension-font-family";
-import TextAlign from "@tiptap/extension-text-align";
 import Typography from "@tiptap/extension-typography";
+import BubbleMenu from "@tiptap/extension-bubble-menu";
+import Placeholder from "@tiptap/extension-placeholder";
+import { cn } from "@/lib/utils";
+import {
+  LuGripVertical as HandleIcon,
+  LuPlus as PlusIcon,
+} from "react-icons/lu";
 
-import { all, createLowlight } from "lowlight";
+type Levels = 1 | 2 | 3;
+const classes: Record<PropertyKey, string> = {
+  1: "h1 text-4xl my-3",
+  2: "h2 text-2xl my-2",
+  3: "h3 text-lg my-1",
+};
 
-// create a lowlight instance with all languages loaded
+interface DragHandleProps {
+  lineDragHandleClass?: string;
+  lineAddHandleClass?: string;
+  lineContainerClass?: string;
+}
+
+const HANDLE_WITDH = 52;
+const MENU_LINE_GAP_WIDTH = 12;
+
+const LineMenu = ({
+  lineDragHandleClass,
+  lineAddHandleClass,
+  lineContainerClass = "items-center",
+}: DragHandleProps) => {
+  const DragHandle = () => (
+    <div
+      className={cn("drag-handle flex items-center", lineDragHandleClass)}
+      contentEditable={false}
+      draggable="true"
+      onDragStart={(event) => {
+        // 드래그 시작 로직 추가 가능
+        console.log("Drag started!", event);
+      }}
+    >
+      <HandleIcon />
+    </div>
+  );
+
+  const AddHandle = () => (
+    <div className={cn("add-handle flex items-center", lineAddHandleClass)}>
+      <PlusIcon />
+    </div>
+  );
+
+  return (
+    <div
+      className={cn(
+        `flex justify-between bg-red py-3`,
+        `w-[${HANDLE_WITDH}px]`,
+        lineContainerClass,
+      )}
+    >
+      <DragHandle />
+      <AddHandle />
+    </div>
+  );
+};
+
+const DraggableNodeView: React.FC<NodeViewProps> = ({ node }) => {
+  const {
+    NodeViewWrapperClass, // 전체 컨테이너 클래스
+    nodeViewClass, // 실제 컨텐츠 클래스
+
+    lineContainerClass, // 메뉴 컨테이너 클래스
+    lineDragHandleClass, // 드래그 핸들메뉴 클래스
+    lineAddHandleClass, // 추가 핸들메뉴 클래스
+
+    translateXValue = HANDLE_WITDH + MENU_LINE_GAP_WIDTH,
+    level, // Heading의 경우
+  } = node.attrs;
+
+  return (
+    <NodeViewWrapper
+      className={`draggable-item flex -translate-x-[${translateXValue}px] gap-3 ${NodeViewWrapperClass}`}
+    >
+      <LineMenu
+        lineDragHandleClass={lineDragHandleClass}
+        lineAddHandleClass={lineAddHandleClass}
+        lineContainerClass={lineContainerClass}
+      />
+      {/* 노드의 실제 콘텐츠 */}
+      <NodeViewContent
+        className={`node-content prose flex ${nodeViewClass} ${!!level && classes[level]}`}
+      />{" "}
+    </NodeViewWrapper>
+  );
+};
+
+const CustomHeading = Heading.configure({
+  levels: [1, 2, 3],
+}).extend({
+  addAttributes(this) {
+    return {
+      lineDragHandleClass: {
+        default: () => {
+          return "flex items-center";
+        },
+      },
+    };
+  },
+
+  addNodeView(this) {
+    return ReactNodeViewRenderer(DraggableNodeView);
+  },
+
+  renderHTML({ node }) {
+    const hasLevel = this.options.levels.includes(node.attrs.level);
+    const level: Levels = hasLevel ? node.attrs.level : this.options.levels[0];
+
+    return [
+      `h${level}`,
+      {
+        class: `${classes[level]}`,
+      },
+      0,
+    ];
+  },
+});
+
+/**
+ * BULLET LIST
+ */
+const CustomBulletList = BulletList.extend({
+  addKeyboardShortcuts() {
+    return {
+      "Mod-l": () => this.editor.commands.toggleBulletList(),
+    };
+  },
+  addNodeView() {
+    return ReactNodeViewRenderer(DraggableNodeView);
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return [
+      "div",
+      { class: "flex items-center gap-2" },
+      ["div", { class: "bullet-symbol flex" }, "•"], // 커스텀 불릿 심볼
+      0, // 리스트 텍스트 삽입
+    ];
+  },
+});
+
+const CustomListItemView = () => {
+  return (
+    <NodeViewWrapper as="li" className="flex gap-2">
+      <span className="bullet-symbol">•</span>
+      <NodeViewContent className="flex-grow" />
+    </NodeViewWrapper>
+  );
+};
+export const CustomListItem = ListItem.extend({
+  renderHTML({ HTMLAttributes }) {
+    return [
+      "li",
+      { ...HTMLAttributes, class: "custom-list-item flex items-center gap-2" },
+      ["span", { class: "bullet-symbol" }, "•"],
+      0,
+    ];
+  },
+
+  addNodeView() {
+    return ReactNodeViewRenderer(CustomListItemView);
+  },
+});
+
+/**
+ * IMAGE
+ */
+const CustomImage = Image.configure({
+  allowBase64: true,
+});
+
+/**
+ * TASKLIST & TASKITEM
+ */
+const CustomTaskList = TaskList.extend({
+  renderHTML({ HTMLAttributes }) {
+    return [
+      "ul",
+      mergeAttributes(this.options.HTMLAttributes, {
+        class: `list-none p-0 m-0`,
+      }),
+      0,
+    ];
+  },
+});
+const CustomTaskItem = TaskItem.configure({
+  nested: true,
+  HTMLAttributes: {
+    class: "flex gap-1.5 m-0 p-0",
+  },
+}).extend({
+  renderHTML({ HTMLAttributes }) {
+    return ["li", { class: "bg-red" }, 0];
+  },
+});
+
+/**
+ * CODEBLOCK
+ */
 const lowlight = createLowlight(all);
+const CustomCodeBlock = CodeBlockLowlight.configure({
+  lowlight,
+  defaultLanguage: "typescript",
+}).extend({
+  addAttributes(this) {
+    return {
+      nodeViewClass: {
+        default: () => {
+          return "p-2 w-full bg-gray-800 text-gray-1";
+        },
+      },
+    };
+  },
+  addNodeView() {
+    return ReactNodeViewRenderer(DraggableNodeView);
+  },
+});
+
+/**
+ * LINK
+ */
+const CustomLink = Link.configure({
+  protocols: ["ftp", "mailto"],
+  defaultProtocol: "https",
+  HTMLAttributes: {
+    rel: "noopener noreferrer",
+    target: null,
+  },
+});
+
+const CustomPlaceholder = Placeholder.configure({
+  placeholder: "Now, a Legend is being written by you...!",
+  emptyEditorClass: "is-editor-empty", // @Base에서 해당 class에 대한 기본 css 적용
+});
+
+const TableComponents = [
+  Table.configure({
+    resizable: true,
+  }),
+  TableRow,
+  TableHeader,
+  TableCell,
+];
 
 export default function BlogEdit() {
   const editor = useEditor({
     extensions: [
       StarterKit,
+
       /**
        * MARK
        */
       Bold,
-      Code,
       Highlight.configure({
         multicolor: true,
       }),
       Italic,
-      Link.configure({
-        protocols: ["ftp", "mailto"],
-        defaultProtocol: "https",
-        HTMLAttributes: {
-          // Change rel to different value
-          // Allow search engines to follow links(remove nofollow)
-          rel: "noopener noreferrer",
-          // Remove target entirely so links open in current tab
-          target: null,
-        },
-      }),
+      CustomLink,
       Strike,
-      TextStyle.configure({ mergeNestedSpanStyles: true }),
+      TextStyle.configure({ mergeNestedSpanStyles: false }),
       Underline,
+
       /**
        * NODES
        */
       Blockquote,
-      BulletList,
-      // CodeBlock,
-      CodeBlockLowlight.configure({
-        lowlight,
-        defaultLanguage: "typescript",
-      }),
-
-      Document,
-      Heading.configure({
-        levels: [1, 2, 3, 4, 5],
-      }),
-      HorizontalRule,
-      Image.configure({
-        allowBase64: true,
-      }),
-      ListItem,
       OrderedList,
 
-      Table.configure({
-        resizable: true,
-      }),
-      TableRow,
-      TableHeader,
-      TableCell,
+      Document,
+      HorizontalRule,
+      CustomImage,
+      CustomHeading,
+      CustomTaskList,
+      CustomTaskItem,
+      CustomCodeBlock,
+      CustomPlaceholder,
+      CustomBulletList,
+      CustomListItem,
 
-      TaskList,
-      TaskItem.configure({
-        nested: true,
-      }),
+      ...TableComponents,
+
       Youtube,
 
       Text,
-      Paragraph,
+      Paragraph.configure({
+        HTMLAttributes: {
+          class: "-translate-y-px m-0",
+        },
+      }),
 
       /**
        * FUNCTIONS
@@ -128,25 +362,28 @@ export default function BlogEdit() {
       Color,
       Dropcursor,
       ListKeymap,
-      FontFamily.configure({
-        types: ["textStyle"],
-      }),
-      TextAlign.configure({
-        types: ["heading", "paragraph"],
-      }),
       Typography,
+      BubbleMenu,
     ],
+    immediatelyRender: false,
     editorProps: {
       attributes: {
-        class: "prose m-5 focus:outline-none leading-normal",
+        class:
+          "prose p-5 leading-normal min-h-40 focus:outline-none max-w-full",
+        //
       },
     },
+    content: `
+      <p>This is a basic example of implementing images. Drag to re-order.</p>
+      <img src="https://placehold.co/800x400" />
+      <img src="https://placehold.co/800x400/6A00F5/white" />
+    `,
   });
 
   return (
     <main className="grid grid-cols-8 gap-x-[1.5vw] px-[10vw]">
       <div className="col-start-2 col-end-8 grid h-fit grid-cols-6 gap-x-[1.5vw] border">
-        <div className="col-start-2 col-end-6 h-40 border">
+        <div className="col-start-2 col-end-6 grid grid-cols-1 border">
           <EditorContent editor={editor} />
         </div>
       </div>
