a718540bf72d6be15502c90226dde49ab3f9cc51
Bumang-Cyber <calmness0729@gmail.com>
2025-02-22 22:12:25 +0900

    feat: getButtonStyle에 flexBoxClass 추가, fillButton wrapper 스타일 재사용, 리스트 - write 버튼 액션버튼으로 전환

--- src/components/pages/blog/blogTitle/index.tsx
+++ src/components/pages/blog/blogTitle/index.tsx
@@ -1,7 +1,12 @@
 import { LuPlus } from "react-icons/lu";
 import Link from "next/link";
+import { FillButton } from "@/components/common";
+import { getButtonColorStyle } from "@/utils/styles/filButtonManager";
+import { cn } from "@/utils/cn";
 
 const BlogTitle = () => {
+  const { textStyle, fillStyle, flexBoxClass } = getButtonColorStyle("dark");
+
   return (
     <div className="col-span-3 mb-5 flex h-10 w-full items-center justify-between font-semibold">
       {/* PAGE BlogTITLE */}
@@ -10,12 +15,9 @@ const BlogTitle = () => {
         <span className="text-sm text-gray-200">28</span>
       </div>
       {/* NEW POST */}
-      <Link
-        href="/blog/edit"
-        className="flex h-8 items-center rounded-md bg-gray-1 px-4 py-2 text-sm font-normal text-gray-700 transition-colors hover:bg-gray-5"
-      >
-        <LuPlus className="-translate-x-1.5" />
-        <span>Write</span>
+      <Link href="/blog/edit" className={cn(flexBoxClass, fillStyle)}>
+        <LuPlus className={textStyle} />
+        <span className={textStyle}>Write</span>
       </Link>
     </div>
   );
