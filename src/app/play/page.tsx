5358e6513dcf6497c331e957a8c3d93e4213860a
Bumang-Cyber <calmness0729@gmail.com>
2025-03-01 23:40:42 +0900

    feat: playItem 레이아웃 설정

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -2,26 +2,19 @@ import { PlayItem } from "@/components/pages";
 
 export default function Play() {
   return (
-    <main className="mx-[10vw] grid grid-cols-6 gap-[1.5vw] bg-red">
-      {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => {
-        if (
-          item === 1 ||
-          item === 2 ||
-          item === 3 ||
-          item === 5 ||
-          item === 7
-        ) {
-          return (
-            <PlayItem
-              key={item}
-              className="bg-blue"
-              title={item.toString()}
-              content={item.toString()}
-            />
-          );
-        } else {
-          return <div key={item} />;
-        }
+    <main className="mx-[10vw] grid grid-cols-6 gap-x-[1.5vw] gap-y-[3vw] bg-red">
+      {[1, null, 2, 3, null, 4, 5, 6, 7, null, 8].map((item) => {
+        if (!item) return <div key={item} />;
+
+        return (
+          <PlayItem
+            key={item}
+            className="bg-blue"
+            height={item * 25}
+            title={item.toString()}
+            content={item.toString()}
+          />
+        );
       })}
     </main>
   );
