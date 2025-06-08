8da7d549ab645cbb21b387063e4d89c1a43ee6e5
Bumang-Cyber <calmness0729@gmail.com>
2025-06-08 22:39:36 +0900

    fix: tag title => label로 변경

--- src/components/layout/sideBar/tags/tagWrapper/index.tsx
+++ src/components/layout/sideBar/tags/tagWrapper/index.tsx
@@ -19,7 +19,7 @@ const TagWrapperLayer = ({ tags }: TagWrapperLayerProps) => {
           <Tag
             key={tag.id}
             id={tag.id}
-            title={tag.title}
+            title={tag.label}
             type="link"
             isActivated={tagIds.includes(`${tag.id}`)}
             hasXButton
