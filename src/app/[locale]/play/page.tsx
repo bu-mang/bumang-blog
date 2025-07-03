0c8b54c1e522a88c6454a85ac2588cbb30677c8d
Bumang-Cyber <calmness0729@gmail.com>
2025-07-04 00:20:23 +0900

    feat: 갤러리 grid 텍스트 맞춤

--- src/app/[locale]/play/page.tsx
+++ src/app/[locale]/play/page.tsx
@@ -2,16 +2,17 @@ import { PlayItem } from "@/components/pages";
 
 export default function Play() {
   return (
-    <main className="mx-[10vw] grid grid-cols-6 gap-x-[1.5vw] gap-y-[3vw] bg-red">
+    <main className="mx-[10vw] grid grid-cols-1 gap-x-[1.5vw] gap-y-[3vw] sm:grid-cols-2 lg:grid-cols-4">
       {[1, null, 2, 3, null, 4, 5, 6, 7, null, 8].map((item) => {
         if (!item) return <div key={item} />;
 
         return (
           <PlayItem
             key={item}
-            className="bg-blue"
-            height={item * 25}
-            title={item.toString()}
+            // className="bg-blue"
+            width={736}
+            height={736}
+            title={"1skdmqwkdmqwn djkqwndjk qwndjk qwndjkqw"}
             content={item.toString()}
           />
         );
--- src/components/pages/play/playItem/index.tsx
+++ src/components/pages/play/playItem/index.tsx
@@ -4,33 +4,105 @@ import { ButtonBase } from "@/components/common";
 import ExpandModal from "@/components/modal/type/expand";
 import useModalStore from "@/store/modal";
 import { cn } from "@/utils/cn";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 interface PlayItemProps {
-  title: string;
-  content: string;
+  title?: string;
+  content?: string;
+  width: number;
   height: number;
   className?: string;
 }
 
-const PlayItem = ({ title, content, height, className }: PlayItemProps) => {
+const PlayItem = ({
+  title,
+  content,
+  width,
+  height,
+  className,
+}: PlayItemProps) => {
   const openModal = useModalStore((state) => state.openModal);
   const handleClick = () => {
     openModal(ExpandModal, {
       title: "?",
     });
   };
+  const textHeight = (title ? 1 : 0) + (content ? 1 : 0);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const [containerWidth, setContainerWidth] = useState(200);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const handleResize = () => {
+      const width = ref.current?.getBoundingClientRect().width;
+      setContainerWidth((width ?? 200) * 0.8);
+    };
+
+    ref.current.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      ref.current?.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <button onClick={handleClick} className="group flex">
+    <button onClick={handleClick} className="group flex flex-1">
       <div
+        ref={ref}
         className={cn(
-          "flex-1 transition-all group-hover:-translate-y-2",
+          // "flex-1 transition-all group-hover:-translate-y-2",
+          "flex flex-1 flex-col justify-center transition-all",
           className,
         )}
-        style={{ height }}
+        style={{ aspectRatio: `${width} / ${height}` }}
       >
-        <span>{title}</span>
-        <span>{content}</span>
+        {/* IMAGE */}
+        <div className="relative flex flex-1">
+          <Image
+            src={"/star.webp"}
+            alt={title ?? "GalleryImage"}
+            className="object-contain"
+            fill
+          />
+        </div>
+
+        {/* DESC */}
+        <div
+          className={cn(
+            "flex-0 flex h-0 w-full min-w-0 flex-col overflow-hidden opacity-0 transition-all group-hover:opacity-100",
+            textHeight === 1 && "group-hover:h-5",
+            textHeight === 2 && "group-hover:h-10",
+          )}
+          style={{
+            transition: "opacity 0.5s ease-in, height 0.5s ease-out",
+          }}
+        >
+          {title && (
+            <div className="flex h-5 w-full items-center justify-center text-sm">
+              <span
+                className="truncate text-center"
+                style={{ width: containerWidth }}
+              >
+                {title}
+              </span>
+            </div>
+          )}
+          {content && (
+            <div className="flex h-5 w-full items-center justify-center text-sm text-gray-300">
+              <span
+                className="truncate text-center"
+                style={{ width: containerWidth }}
+              >
+                {content}
+              </span>
+            </div>
+          )}
+        </div>
       </div>
     </button>
   );
