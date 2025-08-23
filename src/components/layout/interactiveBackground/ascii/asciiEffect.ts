26bb11936987b36d4b6531856283214470e8a621
Bumang-Cyber <calmness0729@gmail.com>
2025-08-24 02:11:57 +0900

    fix: scaleX 윈도우에선 적용하지 않음

--- src/components/layout/interactiveBackground/ascii/asciiEffect.ts
+++ src/components/layout/interactiveBackground/ascii/asciiEffect.ts
@@ -26,14 +26,14 @@ class ASCIIEffect {
     // ASCII 출력용 DOM 요소 생성
     this.domElement = document.createElement("div");
     this.domElement.style.fontFamily = "windo Mono, monospace";
-    // this.domElement.style.fontSize = "8px";
-    // this.domElement.style.lineHeight = "8px";
-    this.domElement.style.fontSize = isWindows ? "7px" : "8px";
-    this.domElement.style.lineHeight = isWindows ? "7px" : "8px";
+    this.domElement.style.fontSize = "8px";
+    this.domElement.style.lineHeight = "8px";
+    // this.domElement.style.fontSize = isWindows ? "7px" : "8px";
+    // this.domElement.style.lineHeight = isWindows ? "7px" : "8px";
     this.domElement.style.whiteSpace = "pre";
     this.domElement.style.color = "#b4b4b4";
 
-    this.domElement.style.transform = "scaleX(1.5)"; // 가로만 2배 확대
+    this.domElement.style.transform = isWindows ? "" : "scaleX(1.5)"; // 가로만 2배 확대
     this.domElement.style.transformOrigin = "left top";
 
     // 렌더 타겟 설정
